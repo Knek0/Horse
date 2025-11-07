dd60ad40191b2ed96ed32b346489503912d06b5e
Knek0 <kiarash.nekonam@outlook.com>
2025-11-07 11:34:26 +0000

    Map Designing
    : )

---
+++ Horse/Assets/ithappy/Animals_FREE/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Coin : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+    }
+}
